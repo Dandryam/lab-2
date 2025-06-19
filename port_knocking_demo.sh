56046633023e8144ce994b04b3e2d1b4691107dc
Dandryam <dandryamin@mail.ru>
2025-06-19 20:39:55 +0500

    deleted useless functional

--- port_knocking_demo.sh
+++
@@ -1,9 +0,0 @@
-# Клиентская машина
-# Попытка подключения без knocking (должна быть отклонена)
-ssh root@localhost -p 2222
-
-# Выполнение knocking
-knock -d 500 -v localhost 7000 8000 9000
-
-# Успешное подключение после knocking (в течение 30 сек)
-ssh root@localhost -p 2222
