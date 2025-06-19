189fdf7cd1cf3389c4351ae7a0e28e4657513aa8
Dandryam <147478845+Dandryam@users.noreply.github.com>
2025-06-19 20:19:11 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -14,6 +14,16 @@ iptables -A INPUT -p udp --dport 7000 -j ACCEPT
 iptables -A INPUT -p udp --dport 8000 -j ACCEPT
 iptables -A INPUT -p udp --dport 9000 -j ACCEPT
 
+# защита от spoofing
+iptables -A INPUT -s 127.0.0.0/8 -j DROP
+iptables -A INPUT -s 10.0.0.0/8 -j DROP
+iptables -A INPUT -s 172.16.0.0/12 -j DROP
+iptables -A INPUT -s 192.168.0.0/16 -j DROP
+
+# Ограничить rate-limit для knocking портов
+iptables -A INPUT -p udp --dport 7000 -m limit --limit 1/min -j ACCEPT
+iptables -A INPUT -p udp --dport 7000 -j DROP
+
 # Запуск сервисов
 service knockd start
 service fail2ban start
